1c52de73a21646e75c7f7fc0f252a263a83aa11f
Benjamin Gales <bjmgales@gmail.com>
2024-10-22 12:02:45 +0200

    add: remove duplicate from the union table "customers"

---
+++ Data Warehouse/ex02/remove_duplicates.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+
+def create_tmp_table_replace_old(cursor: object, connection: object):
+    cursor.execute('''
+        CREATE TABLE temp_table (LIKE customers INCLUDING ALL);
+    ''')
+
+    cursor.execute('''
+        INSERT INTO temp_table SELECT DISTINCT * FROM customers;
+    ''')
+
+    cursor.execute('''
+        DROP TABLE customers;
+    ''')
+
+    cursor.execute('''
+        ALTER TABLE temp_table RENAME TO customers;
+    ''')
+    connection.commit()
+
+
+def rem_duplicate_sql_table():
+    try:
+        connection = psycopg2.connect(
+            database='piscineds',
+            user='bgales',
+            password='mysecretpassword',
+            host='localhost',
+        )
+        cursor = connection.cursor()
+        create_tmp_table_replace_old(cursor, connection)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print(f"Error : {e}")
+        return
+if __name__ == '__main__':
+    rem_duplicate_sql_table()
