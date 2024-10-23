1a602f0074014b1442d43b582f0d1067b6cc22a6
Benjamin Gales <bjmgales@gmail.com>
2024-10-23 17:07:54 +0200

    add: function to delete all tables and restart from scratch

---
+++ utils.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+connect = psycopg2.connect(
+    database='piscineds',
+    user='bgales',
+    password='mysecretpassword',
+    host='localhost'
+)
+cursor = connect.cursor()
+
+
+def del_all_tables():
+    cursor.execute(
+        '''
+            SELECT table_name FROM information_schema.tables
+            WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
+        '''
+    )
+    for table in cursor.fetchall():
+        cursor.execute(f'DROP TABLE {table[0]} CASCADE;')
+        print(f'Deleted {table[0]}')
+    connect.commit()
+
+if __name__ == '__main__':
+    del_all_tables()
