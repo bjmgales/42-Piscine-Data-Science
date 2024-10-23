203fc1f1fe9158c6259054633a772e067ba428f9
Benjamin Gales <bjmgales@gmail.com>
2024-10-23 22:26:49 +0200

    add: log messages

--- Data engineer/ex02/table.py
+++ Data engineer/ex02/table.py
@@ -3,6 +3,7 @@ import os
 
 
 def create_new_table(cursor: object, table_name: str, file_path: str):
+    print(f'\033[93mcreating table {table_name}...')
     table_creation = f'''
            CREATE TABLE {table_name} (
            event_time TIMESTAMPTZ,
@@ -14,9 +15,12 @@ def create_new_table(cursor: object, table_name: str, file_path: str):
        )
        '''
     cursor.execute(table_creation)
+    print(f'\033[92mtable {table_name} created with success!')
     with open(file_path, 'r') as csv_buf:
+        print(f'\033[93mcopying {file_path} into {table_name}...')
         next(csv_buf)
         cursor.copy_from(csv_buf, table_name, sep=',')
+        print('\033[92mcopy made with success!')
     csv_buf.close()
 
 
@@ -44,26 +48,33 @@ def data_csv_to_db(file_path: str):
 
     try:
         table_name = os.path.splitext(os.path.basename(file_path))[0]
+        print("\033[95mconnection to database...")
         connection = psycopg2.connect(
             database="piscineds",
             user="bgales",
             password="mysecretpassword",
             host='localhost'
         )
+        print('\033[92mconnection with database OK')
+
         cursor = connection.cursor()
-
+        cursor.execute("BEGIN;")
         if is_table_existing(cursor, table_name) is False:
-            print(f'table {table_name} does not exists. Creating table...')
             create_new_table(cursor, table_name, file_path)
+            print('\033[93mcomitting changes...')
             connection.commit()
+            print("\033[92mcommited with success!")
         else:
-            print(f'table {table_name} already exists.')
+            print(f'\033[93mtable {table_name} already exists.')
 
+    except Exception as e:
+        print("\033[91mError :", e)
+        print('\033[93mrollback in progress...')
+        connection.rollback()
+    finally:
+        print("\033[95mclosing connection with database...")
         connection.close()
         cursor.close()
-    except Exception as e:
-        print("Error :", e)
-    return
 
 
 if __name__ == "__main__":
